878b0979a61389e80633283eac834897032223c9
Travis Shirley <1431anonymouse@gmail.com>
2023-08-21 12:29:35 -0400

    Features added - can now change settings and rerun app - reorganized GUI to look nicer - can now change which action is controlled by which gesture - added default values for a whole bunch of settings

--- gestureReader.py
+++ gestureReader.py
@@ -11,15 +11,22 @@ GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
 GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
-paused = False   # Bool for pausing reading input
-request_stop = False    # Bool for when to stop the app
+paused = False  # Bool for pausing reading input
+request_stop = False  # Bool for when to stop the app
 did_read = False  # Controls delays in reading more gestures to prevent repeats
 process_delay = 10  # Used to adjust target FPS due to processing delays
 one_sec = 1000  # Constant for 1 second
 keyboard = Controller()  # Used to control inputs
-volume_change = 2  # Used to control how much volume changes by
-def_read_delay = 1  # Default read delay
-def_fps = 30    # Default target fps
+volume_change = "2"  # Used to control how much volume changes by
+def_read_delay = "1"  # Default read delay
+def_fps = "30"  # Default target fps
+def_volume_up = "Thumbs Up"  # Default volume up gesture
+def_volume_down = "Thumbs Down"  # Default volume down gesture
+def_play_pause = "Open Palm"  # Default play/pause gesture
+def_stop_cam = "Closed Fist"  # Default stop camera gesture
+def_pause_cam = "Victory/Peace Sign"  # Default pause reading gesture
+gestures_display = ["Thumbs Up", "Thumbs Down", "Open Palm", "Closed Fist", "Victory/Peace Sign"]
+gestures_real = ["Thumb_Up", "Thumb_Down", "Open_Palm", "Closed_Fist", "Victory"]
 
 
 def process_result(result: GestureRecognizerResult, useless1, useless2):
@@ -34,13 +41,13 @@ def process_result(result: GestureRecognizerResult, useless1, useless2):
             print('DEBUG: gesture recognition result: {}'.format(result))
         if not paused:
             print('DEBUG: gesture recognition result: {}'.format(result))
-            if gesture == "Thumb_Up":
-                for x in range(volume_change):
+            if gesture == def_volume_up:
+                for x in range(int(volume_change)):
                     keyboard.press(Key.media_volume_up)
                     keyboard.release(Key.media_volume_up)
                     time.sleep(0.01)
-            elif gesture == "Thumb_Down":
-                for x in range(volume_change):
+            elif gesture == def_volume_down:
+                for x in range(int(volume_change)):
                     keyboard.press(Key.media_volume_down)
                     keyboard.release(Key.media_volume_down)
                     time.sleep(0.01)
@@ -55,25 +62,32 @@ def return_camera_indexes():
     # checks the first 10 indexes for viable cameras to use
     index = 0
     arr = []
-    i = 10
-    while i > 0:
+    r = 10
+    while r > 0:
         cap = cv.VideoCapture(index)
         if cap.read()[0]:
             arr.append(str(index))
             cap.release()
         index += 1
-        i -= 1
+        r -= 1
     return arr
 
 
-def run(sel_camera, target_fps, delay_read, volume_change_val):
+def run(sel_camera, target_fps, delay_read, volume_change_val, volume_up, volume_down, play_pause,
+        stop_cam, pause_cam):
+    global def_volume_up, def_volume_down, def_play_pause, def_stop_cam, def_pause_cam
+    def_volume_up = volume_up
+    def_volume_down = volume_down
+    def_play_pause = play_pause
+    def_stop_cam = stop_cam
+    def_pause_cam = pause_cam
     global volume_change
     volume_change = volume_change_val
     camera = sel_camera.removeprefix("Camera ")
     cam = cv.VideoCapture(int(camera))
     old_time = 0
     timer = 0
-    global did_read
+    global did_read, request_stop
     with GestureRecognizer.create_from_options(options) as recognizer:
         while not request_stop:
             timer += 1
@@ -92,8 +106,9 @@ def run(sel_camera, target_fps, delay_read, volume_change_val):
                 did_read = False
             else:
                 cv.waitKey(round(1000 / target_fps) - process_delay)
-        recognizer.close()
     cam.release()
+    cv.destroyAllWindows()
+    request_stop = False
 
 
 options = GestureRecognizerOptions(
@@ -115,12 +130,37 @@ camera_sel_dropdown = ttk.Combobox(main, textvariable=camera_sel,
                                    values=cameras)
 camera_sel_dropdown.grid(row=1, column=0)
 
-tk.Label(main, text="Gesture Usage:").grid(row=2, column=0)
-# Placeholder for now
-option_2_var = tk.StringVar(value="Option A")
-dropdown_2 = ttk.Combobox(main, textvariable=option_2_var,
-                          values=["Option A", "Option B", "Option C"])
-dropdown_2.grid(row=3, column=0)
+tk.Label(main, text="Gesture Usage:").grid(row=0, column=2)
+# Drop downs for changing which gesture does what
+tk.Label(main, text="Volume Up:").grid(row=1, column=1)
+volume_up_gesture = tk.StringVar(value=def_volume_up)
+volume_up_dropdown = ttk.Combobox(main, textvariable=volume_up_gesture,
+                                  values=gestures_display)
+volume_up_dropdown.grid(row=1, column=3)
+
+tk.Label(main, text="Volume Down:").grid(row=2, column=1)
+volume_down_gesture = tk.StringVar(value=def_volume_down)
+volume_down_dropdown = ttk.Combobox(main, textvariable=volume_down_gesture,
+                                    values=gestures_display)
+volume_down_dropdown.grid(row=2, column=3)
+
+tk.Label(main, text="Play/Pause:").grid(row=3, column=1)
+play_pause_gesture = tk.StringVar(value=def_play_pause)
+play_pause_dropdown = ttk.Combobox(main, textvariable=play_pause_gesture,
+                                   values=gestures_display)
+play_pause_dropdown.grid(row=3, column=3)
+
+tk.Label(main, text="Close Camera:").grid(row=4, column=1)
+stop_cam_gesture = tk.StringVar(value=def_stop_cam)
+stop_cam_dropdown = ttk.Combobox(main, textvariable=stop_cam_gesture,
+                                 values=gestures_display)
+stop_cam_dropdown.grid(row=4, column=3)
+
+tk.Label(main, text="Pause/Unpause Program:").grid(row=5, column=1)
+pause_cam_gesture = tk.StringVar(value=def_pause_cam)
+pause_cam_dropdown = ttk.Combobox(main, textvariable=pause_cam_gesture,
+                                  values=gestures_display)
+pause_cam_dropdown.grid(row=5, column=3)
 
 tk.Label(main, text="Target FPS:").grid(row=4, column=0)
 # FPS target field
@@ -135,7 +175,7 @@ read_delay.insert(0, def_read_delay)
 read_delay.grid(row=8, column=0)
 
 tk.Label(main, text="Volume Change:").grid(row=9, column=0)
-tk.Label(main, text="Volume change is a multiple of 2. For example, volume change of 3 means"
+tk.Label(main, text="Volume change is a multiple of 2.\nFor example, volume change of 3 means\n"
                     " volume changes by 6 on increase/decrease.").grid(row=10, column=0)
 # Volume change field
 volume_change_field = tk.Entry(main)
@@ -146,9 +186,28 @@ volume_change_field.grid(row=11, column=0)
 start_button = tk.Button(main, text="Start", command=lambda: run(camera_sel.get(),
                                                                  int(fps_target.get()),
                                                                  int(read_delay.get()),
-                                                                 int(volume_change_field.get())
+                                                                 int(volume_change_field.get()),
+                                                                 gestures_real[gestures_display
+                                                                 .index(
+                                                                     str(volume_up_gesture.get()))],
+                                                                 gestures_real[gestures_display
+                                                                 .index(
+                                                                     str(volume_down_gesture.get()
+                                                                         ))],
+                                                                 gestures_real[gestures_display
+                                                                 .index(
+                                                                     str(play_pause_gesture.get()
+                                                                         ))],
+                                                                 gestures_real[gestures_display
+                                                                 .index(
+                                                                     str(stop_cam_gesture.get()
+                                                                         ))],
+                                                                 gestures_real[gestures_display
+                                                                 .index(
+                                                                     str(pause_cam_gesture.get()
+                                                                         ))]
                                                                  ))
-start_button.grid(row=12, column=0)
+start_button.grid(row=12, column=1)
 
 # Run the GUI event loop
 main.mainloop()
